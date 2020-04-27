fca1ecc71101ebd50cbf03893be7a062cfbf8ccb
Igor-Felipy <igor.felipy123@gmail.com>
2020-04-27 18:15:01 -0400

    foi adicionado:  jogar mais de um dado por vez!

--- Simulador_Dados.py
+++ Simulador_Dados.py
@@ -4,10 +4,11 @@ print('Bem-vindo ao simulador de dados para jogos')
 while True:
     lados = int(input('Entre com o valor de lados do seu dado: '))
     qtd = int(input('Quantos dados voce vai jogar? '))
+    rol = 0
+    for i in range(0, qtd):
+        rol = rol + randint(1, lados)
 
-    rol = randint(1, lados)
-
-    bonus = input('Você tem algum bonûs?? ')
+    bonus = input('Você tem algum bonûs??  [S/N] ')
     if bonus.upper() == 'S':
         bo = int(input('coloque o seu bonûs: '))
         rol = rol + bo
@@ -18,3 +19,9 @@ while True:
         continue
 
     print(rol)
+    while True:
+        reiniciar = input('Continuar? [S/N] ')
+        if reiniciar.upper() == 'S':
+            break
+        else:
+            continue
