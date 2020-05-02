68a8379326f4ea5bfeb553a34537f83aa35ad3bc
Igor-Felipy <igor.felipy123@gmail.com>
2020-05-02 14:00:18 -0400

    atualizado para integrar uma interface grafica

--- Simulador_Dados.py
+++ Simulador_Dados.py
@@ -1,27 +1,38 @@
 from random import randint
+import PySimpleGUI as sg
 
-print('Bem-vindo ao simulador de dados para jogos')
-while True:
-    lados = int(input('Entre com o valor de lados do seu dado: '))
-    qtd = int(input('Quantos dados voce vai jogar? '))
-    rol = 0
-    for i in range(0, qtd):
-        rol = rol + randint(1, lados)
+class GUI:
+    def __init__(self):
+        #layout
+        layout = [
+            [sg.Text('Quantidade de lados'),sg.Slider(range = (4,250),default_value=4,orientation='h',key='lados')],
+            [sg.Text('Quantidade de dados'),sg.Slider(range = (1,50),default_value=1,orientation = 'h',key='qtd')],
+            [sg.Text('Voce tem bonus?'),sg.Radio('Sim','Bonus',key='S'),sg.Radio('Não','Bonus',key='N')],
+            [sg.Text('Quantidade de bonus'),sg.Slider(range = (0,200), default_value = 1, orientation = 'h',key='bonus')],
+            [sg.Button('Jogar os Dados')],
+            [sg.Output(size=(45,10))]
+        ]
+        #janela
+        self.janela = sg.Window("Dados virtuais").layout(layout)
 
-    bonus = input('Você tem algum bonûs??  [S/N] ')
-    if bonus.upper() == 'S':
-        bo = int(input('coloque o seu bonûs: '))
-        rol = rol + bo
-    elif bonus.upper() == 'N':
-        print('OK!')
-    else:
-        print('erro!!!')
-        continue
+    def iniciar(self):
+        #extração de dados
+        while True:
+            self.button, self.values = self.janela.Read()
+            bonus = self.values['bonus']
+            lados = self.values['lados']
+            qtd = int(self.values['qtd'])
+            rol = 0
+            b = self.values['S']
+            for i in range(0, qtd):
+                rol = rol + randint(1, lados)
 
-    print(rol)
-    while True:
-        reiniciar = input('Continuar? [S/N] ')
-        if reiniciar.upper() == 'S':
-            break
-        else:
-            continue
+            if b:
+                rol = rol + bonus
+            
+            print('='*20)
+            print(int(rol))
+            print('='*20)
+
+tela = GUI()
+tela.iniciar()
