002d2c8720c984b203b13fbc364f3edc2a7fd0e8
Igor-Felipy <igor.felipy123@gmail.com>
2020-05-04 22:44:54 -0400

    definido layout

--- pedra_papel_tesoura.py
+++ pedra_papel_tesoura.py
@@ -1,2 +1,9 @@
 import PySimpleGUI as sg 
 
+class Tela:
+    def __init__(self):
+        #Layout
+        layout = [
+            [sg.Radio('Pedra','jogo',key='pedra'),sg.Radio('Papel','jogo',key='papel'),sg.Radio('Tesoura','jogo',key='tesoura')]
+        ]
+        
