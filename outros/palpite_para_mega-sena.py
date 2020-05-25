cf6511f16f475b42141ede74b82e9ed8c0871239
Igor-Felipy <igor.felipy123@gmail.com>
2020-05-25 14:50:06 -0400

    refeito e funcionando!

--- outros/palpite_para_mega-sena.py
+++ outros/palpite_para_mega-sena.py
@@ -1,11 +1,25 @@
-from random import choice
-numeros = [
-    1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
-]
-palpites = []
-for i in range(0, 6):
-    palpites = choice(numeros)
+from random import randint
+lista = list()
+jogos = list()
+print('-'*30)
+print('     JOGA NA MEGA SENA       ')
+print('-'*30)
+quant = int(input('Quantos jogos voce quer que eu sorteie: '))
+tot = 0
+while tot < quant:
+    count = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            count+= 1
+        if count >= 6:
+            break
 
-
-for i in range(0,6):
-    print(palpites[i])
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot +=1
+print('-='*3,f'sorteando {quant} jogos','-='*3)
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l} ')
