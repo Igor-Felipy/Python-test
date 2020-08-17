e604f95e36c2f1b0cdefc66d414958c425463287
Igor-Felipy <igor.felipy123@gmail.com>
2020-08-16 21:08:14 -0400

    controlando o mouse

--- outros/MouseController.py
+++ outros/MouseController.py
@@ -1,4 +1,13 @@
 from pynput.mouse import Button, Controller
 
 mouse = Controller()
-print(mouse.position)
+#mostrando posição atual
+#print(mouse.position)
+#movendo para posicao especifica
+#mouse.position = (15, 756)
+#move o mouse em posicao relativa 
+#mouse.move(0,150)
+#clicando no mouse
+#mouse.click(Button.left, 2)
+#mouse.press(Button.left)
+#mouse.scroll(0,-100)
